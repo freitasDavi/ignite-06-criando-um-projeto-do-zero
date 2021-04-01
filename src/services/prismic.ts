567eddfb96e88624dab0452bca0fc7175772a312
Davi <davi.frrs@outlook.com>
2021-03-31 23:47:22 -0300

    Created posts at prismic and generated keys

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -8,3 +8,7 @@ export function getPrismicClient(req?: unknown): DefaultClient {
 
   return prismic;
 }
+
+// Não esquecer de pegar as chaves aqui e colocar no .env.local
+// PRISMIC_API_ENDPOINT=https://spacetravel.cdn.prismic.io/api/v2
+// PRISMIC_ACCESS_TOKEN=MC5ZR1V6VmhJQUFDSUFTcFFK.77-9OGrvv703VwPvv70Q77-977-977-977-9HDjvv73vv70iJyHvv73vv73vv70s77-977-977-9Ee-_ve-_ve-_ve-_vQ
